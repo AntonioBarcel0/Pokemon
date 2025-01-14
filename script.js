f0193033978b100fb23e47de5477cfce7a9862c7
AntonoBarcel0 <antoniogibarber99@gmail.com>
2025-01-14 14:00:35 +0100

    sexto

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const form = document.getElementById
+const form = document.getElementById('pokemon-form');
+const input = document.getElementById('pokemon-name');
+const pokemonInfoDiv = document.getElementById('pokemon-info');
+
+async function fetchPokemonData (params) {
+    
+}
